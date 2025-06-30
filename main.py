072069d4061415c391c2a296ca4d15102090c936
Ransoni4546 <laytonaldridge@gmail.com>
2025-06-30 07:55:29 -0500

    Added ch_dict func

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from stats import get_book_text
 
 from stats import get_num_words
 
+from stats import get_ch_dict
+
 def main():
     print(f"{get_num_words(get_book_text("books/frankenstein.txt"))} words found in the document")
+    print(get_ch_dict(get_book_text("books/frankenstein.txt")))    
 
 main()
--- stats.py
+++ stats.py
@@ -3,6 +3,14 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
-def get_num_words(file_contents):
-    num_words = len(file_contents.split())
+def get_num_words(full_book):
+    num_words = len(full_book.split())
     return num_words
+
+def get_ch_dict(book):
+    lower_case_book = book.lower()
+    ch_dict = {}
+    for ch in lower_case_book:
+        ch_dict[ch] = ch_dict.get(ch, 0) + 1
+    return ch_dict
+    
